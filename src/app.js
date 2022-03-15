e25aec4dd98927c3c79ee704a571eefff3fffb05
Bernardo Rodrigues <bernardo.rodrigues06@unifei.edu.br>
2022-03-15 14:31:05 -0300

    build: add environment variable for server port

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 import express, {json} from "express"
 import cors from "cors"
 import router from "./routes/index.js"
+import config from "./config.js";
 
 const app = express()
 
@@ -9,6 +10,6 @@ app.use(cors())
 app.use(json())
 app.use(router)
 
-app.listen(4000, ()=>{
-    console.log(`Server listening on PORT 4000`)
+app.listen(config.port, ()=>{
+    console.log(`Server listening on PORT ${config.port}`)
 }) 
